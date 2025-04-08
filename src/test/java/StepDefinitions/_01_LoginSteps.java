9bb2175b423658d2eb216ef098ee6a4ef4ef837b
TechnoStudy <ismettechno@gmail.com>
2025-04-08 19:08:24 +0100

    GWD asama 1

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -1,23 +1,29 @@
 package StepDefinitions;
 
+import Utilities.GWD;
 import io.cucumber.java.en.*;
 
 public class _01_LoginSteps {
 
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Siteye gidildi");
+        GWD.getDriver().get("https://test.mersys.io/");
     }
+
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("username ve password girildi");
+        System.out.println("login butonuna tıklatıldı");
+        //sendkeys
+        //GWD.getDriver().sendKeys(element,"username");
+        //login click
     }
+
     @Then("User should login successfully")
     public void user_should_login_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("login olduğumu doğruladım");
+        //assert
     }
 
 
---
+++ src/test/java/Utilities/GWD.java
@@ -0,0 +1,23 @@
+package Utilities;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class GWD {
+   private static WebDriver driver;
+
+   public static WebDriver getDriver()
+   {
+       if (driver == null) { //henüz ilk başta çalışsın
+           driver = new ChromeDriver();
+           driver.manage().window().maximize();
+           driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+       }
+
+       return driver;
+   }
+
+}
