3b7214d6ab7ee125728ac22714297617f71272d7
TechnoStudy <ismettechno@gmail.com>
2025-04-09 19:31:28 +0100

    navigateToCountry locator

--- src/test/java/Pages/LeftNav.java
+++ src/test/java/Pages/LeftNav.java
@@ -7,7 +7,17 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LeftNav extends ParentPage{
 
+    public LeftNav() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
 
+    @FindBy(xpath = "(//span[text()='Setup'])[1]")
+    public WebElement setup;
 
+    @FindBy(xpath = "//span[text()='Parameters']")
+    public WebElement parameters;
+
+    @FindBy(xpath = "(//span[text()='Countries'])[1]")
+    public WebElement countries;
 
 }
--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -1,19 +1,28 @@
 package StepDefinitions;
 
+import Pages.LeftNav;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class _02_CountrySteps {
+    LeftNav ln = new LeftNav();
+
     @And("Navigate to country")
     public void navigateToCountry() {
+        ln.myClick(ln.setup);
+        ln.myClick(ln.parameters);
+        ln.myClick(ln.countries);
     }
 
     @When("Create a country")
     public void createACountry() {
+
     }
 
     @Then("Success message should be displayed")
     public void successMessageShouldBeDisplayed() {
+
     }
+
 }
