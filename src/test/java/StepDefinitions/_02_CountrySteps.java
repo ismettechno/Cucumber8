70cdb3d6a77a3d113b7cf7c24275436281f0e14a
TechnoStudy <ismettechno@gmail.com>
2025-04-09 19:11:00 +0100

    _02_CountrySteps  steps açıldı

---
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _02_CountrySteps {
+    @And("Navigate to country")
+    public void navigateToCountry() {
+    }
+
+    @When("Create a country")
+    public void createACountry() {
+    }
+
+    @Then("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+    }
+}
