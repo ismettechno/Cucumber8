0917896f57ad06ee7344f865fe23e478f5ec34dd
TechnoStudy <ismettechno@gmail.com>
2025-04-17 18:07:49 +0100

    ExtendReport İşlemleri 6

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,14 +2,17 @@ package StepDefinitions;
 
 import Utilities.GWD;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 
 
 public class Hooks {
 
     @After// Cucumber ın Annotation ı
            // her senaryodan sonra otomatik çalışır
-    public void after()
+    public void after(Scenario senaryo)
     {
+
+
         GWD.quitDriver();
     }
 
