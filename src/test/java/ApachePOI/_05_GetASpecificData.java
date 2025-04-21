36f3bdda3eb5a5cd9e6d7600e979e6b37db5c36e
TechnoStudy <ismettechno@gmail.com>
2025-04-21 19:36:30 +0100

    _04_ApachePOIGetAllData

---
+++ src/test/java/ApachePOI/_05_GetASpecificData.java
@@ -0,0 +1,9 @@
+package ApachePOI;
+
+/**
+ * Kullanıcı aşağıdaki excelde, 1.sutundaki bilgilerden istediğini girecek
+ * karşılığında bu bilginin satırındaki karşılık gelen bilgilerin tamamı yazdırılacak.
+ * src/test/java/ApachePOI/resources/LoginData.xlsx
+ */
+public class _05_GetASpecificData {
+}
