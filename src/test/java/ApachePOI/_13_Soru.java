4fbcde48b146d1ea850af6eb1e2a67a24a2394aa
TechnoStudy <ismettechno@gmail.com>
2025-04-28 19:12:37 +0100

    _13_Soru Sorusu

---
+++ src/test/java/ApachePOI/_13_Soru.java
@@ -0,0 +1,26 @@
+package ApachePOI;
+
+
+/*
+   Main den bir metod çağırmak suretiyle, henüz oluşturulmamış
+   path i verilen excel için, program her çalıştıkça içine "test passed"
+   yazısını ekleyen metodu yazınız.
+   Yani dosya yoksa oluştur ilk satıra yaz, sonra dosya oluşacağı için
+   dosya varsa en son satırın altına yaz.
+
+   File dosya= new File(path); //dosya kontrolü
+
+if (!file.exists()) //dosya yok ise
+{
+  oluşturup
+  merhaba düznya yazsın
+}
+else
+{
+   var olan dosyaya eklesin
+  }
+
+ */
+
+public class _13_Soru {
+}
--- src/test/java/Utilities/ExcelUtility.java
+++ src/test/java/Utilities/ExcelUtility.java
@@ -1,6 +1,39 @@
 package Utilities;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 public class ExcelUtility {
+
+    public static ArrayList<ArrayList<String>> getData(String path, String sheetName, int sutunSayisi) {
+        ArrayList<ArrayList<String>> tablo = new ArrayList<>();
+        Sheet sheet=null;
+
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            sheet = workbook.getSheet(sheetName);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {  // tüm satırlardaki
+
+            ArrayList<String> satir = new ArrayList<>();
+            for (int j = 0; j < sutunSayisi; j++) {  // istenen sütun sayısı kadar
+                satir.add(sheet.getRow(i).getCell(j).toString());  // bu döngü ile tüm satır okundu
+            }
+
+            tablo.add(satir);
+        }
+
+        return tablo;
+    }
+
 
 
 }
