46b6cbc3bf28d19e7f41718c09d2a018ce499e34
TechnoStudy <ismettechno@gmail.com>
2025-04-28 17:05:06 +0100

    Öncekinin Tekrarı

--- src/test/java/ApachePOI/_09_Soru.java
+++ src/test/java/ApachePOI/_09_Soru.java
@@ -25,40 +25,6 @@ public class _09_Soru {
     public static void main(String[] args) throws IOException {
         String path="src/test/java/ApachePOI/resource/YeniExcel2.xlsx";
 
-        File dosya=new File(path);
-        if (!dosya.exists())  // dosya yok ise
-        {
-            XSSFWorkbook workbook=new XSSFWorkbook();
-            XSSFSheet sheet= workbook.createSheet("sayfa1");
-
-            Row newRow= sheet.createRow(0);
-            Cell newCell= newRow.createCell(0);
-            newCell.setCellValue("Merhaba Dünya 0");
-
-            FileOutputStream outputStream=new FileOutputStream(path);
-            workbook.write(outputStream);
-            workbook.close();
-            outputStream.close();
-            System.out.println("Dosya oluşturuldu");
-        }
-        else // dosya var ise
-        {
-            FileInputStream inputStream=new FileInputStream(path);
-            Workbook workbook=WorkbookFactory.create(inputStream);
-            Sheet sheet= workbook.getSheetAt(0);
-
-            int satirMiktari=sheet.getPhysicalNumberOfRows(); // satır miktarı
-            Row newRow= sheet.createRow(satirMiktari);
-            Cell newCell= newRow.createCell(0);
-            newCell.setCellValue("Merhaba Dünya "+satirMiktari);
-
-            FileOutputStream outputStream=new FileOutputStream(path);
-            workbook.write(outputStream);
-            workbook.close();
-            outputStream.close();
-            System.out.println("Dosyaya Eklendi");
-        }
-
         //if dosya yok ise yeni exceli oluştur ve ilk satırı yaz
         //else dosya var ise excel in sonuna satır ekle
 
