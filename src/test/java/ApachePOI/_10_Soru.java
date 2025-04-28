3cfe0d0855c1b98e554c33f1f5447af29402c08b
TechnoStudy <ismettechno@gmail.com>
2025-04-28 18:10:06 +0100

    _10_Soru çözümü

--- src/test/java/ApachePOI/_10_Soru.java
+++ src/test/java/ApachePOI/_10_Soru.java
@@ -1,5 +1,11 @@
 package ApachePOI;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Excel Çarpım tablosunu
  * YENİ bir excel dosyasına
@@ -7,17 +13,35 @@ package ApachePOI;
  */
 
 public class _10_Soru {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String path = "src/test/java/ApachePOI/resource/carpimTablosu.xlsx";
 
+        XSSFWorkbook workbook=new XSSFWorkbook();
+        Sheet sheet= workbook.createSheet("Sayfa1");
+
+        int yeniSatirIndex=0;
         for (int i = 1; i < 10; i++) {
 
             for (int j = 1; j < 10; j++) {
-                System.out.println(j+"x"+i+"="+(j*i));
+                Row yeniSatir=sheet.createRow(yeniSatirIndex++);
+                  //System.out.println(j+"x"+i+"="+(j*i)); // 5 hücre
+
+                Cell cell1=yeniSatir.createCell(0); cell1.setCellValue(i);
+                Cell cell2=yeniSatir.createCell(1); cell2.setCellValue("x");
+                Cell cell3=yeniSatir.createCell(2); cell3.setCellValue(j);
+                Cell cell4=yeniSatir.createCell(3); cell4.setCellValue("=");
+                Cell cell5=yeniSatir.createCell(4); cell5.setCellValue(i*j);
             }
 
-            System.out.println();
+            //System.out.println();
+            sheet.createRow(yeniSatirIndex++);
         }
 
-
+        //kaydet
+        FileOutputStream outputStream = new FileOutputStream(path);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+        System.out.println("İşlem tamamlandı");
     }
 }
